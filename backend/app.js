1ae4224219826b90e0834a453a9b072917c83cb0
iamaabhishek <abhishekaydv01@gmail.com>
2025-12-12 23:30:49 +0530

    Basic Setup For TO Do LIST

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import dotenv from "dotenv"
+const app = express()
+
+dotenv.config()
+
+const port = process.env.PORT || 4000
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
